f41bc08361838a0113fd08989adfe2743079334a
marbeydotdev <contact@marbey.dev>
2024-10-29 21:53:38 +0100

    + docker, api

--- src/lib/configuration.ts
+++ src/lib/configuration.ts
@@ -1,7 +1,7 @@
 export const settings = {
 	apiDomain:
 		process.env.NODE_ENV === 'production'
-			? 'https://ggclan-backend-gsaqgrbxamgraxa7.canadacentral-01.azurewebsites.net/v1'
+			? 'https://ggclan.net/api/v1/'
 			: 'http://localhost:5229/v1',
 	redirectUri:
 		process.env.NODE_ENV === 'production' ? 'https://www.ggclan.net' : 'http://localhost:5173'
