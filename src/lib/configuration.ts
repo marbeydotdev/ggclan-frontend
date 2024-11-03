e2fac6f587a572cea67f2b0248bb8d548ef66fe8
marbeydotdev <contact@marbey.dev>
2024-11-03 13:21:04 +0100

    + bad backslash

--- src/lib/configuration.ts
+++ src/lib/configuration.ts
@@ -1,7 +1,7 @@
 export const settings = {
 	apiDomain:
 		process.env.NODE_ENV === 'production'
-			? 'https://ggclan.net/api/v1/'
+			? 'https://ggclan.net/api/v1'
 			: 'http://localhost:5229/v1',
 	redirectUri:
 		process.env.NODE_ENV === 'production' ? 'https://www.ggclan.net' : 'http://localhost:5173'
